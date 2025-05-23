a3f0ec78e53269a3688993df1208acb13fccd66a
SANJAY D <148446978+sanjay-d05@users.noreply.github.com>
2025-05-23 17:28:58 +0530

    Update Footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -25,7 +25,7 @@ function Footer() {
       ] ;
 
   return (
-    <div className='py-3 mt-5' style={{backgroundColor : '#1e1e2e'}}>
+/*     <div className='py-3 mt-5' style={{backgroundColor : '#1e1e2e'}}>
         <div className='d-flex flex-column flex-md-row justify-content-between align-items-center '>
             <p className='mb-0'>
                 Designed And Developed By SANJAY D
@@ -36,6 +36,32 @@ function Footer() {
                   {link.icon}
                 </Link>
               ))}
+            </div>
+        </div>
+    </div> */
+
+       <div className="container-fluid py-3" style={{backgroundColor:'#2a2a36'}}>
+        <div className="container">
+            <div className="row justify-content-center">
+                <div className="col-12 col-md-4 text-center">
+                    <p className='mb-0 text-light'>
+                        Developed By SANJAY D
+                    </p>
+                </div>
+                <div className="col-12 col-md-4 text-center">
+                    <p className='mb-0 text-light'>2024 @ All Rights Reserved</p>
+                </div>
+                <div className="col-12 col-md-4 d-flex gap-3 justify-content-center">
+                    <a className='footer-social-icons' href="https://github.com/sanjay-d05" target='_blank'>
+                        <FaGithub size={20} color='#ab3483'/>
+                    </a>
+                    <a className='footer-social-icons' href="https://www.linkedin.com/in/sanjayd05" target='_blank'>
+                        <FaLinkedinIn size={20} color='#ab3483'/>
+                    </a>
+                    <a className='footer-social-icons' href="https://leetcode.com/u/sanjay_05_08_17" target='_blank'>
+                        <SiLeetcode size={20} color='#ab3483'/>
+                    </a>
+                </div>
             </div>
         </div>
     </div>
